7ef7fc73de31a56fcc2bb6d9430dac316e977327
Eivind Sandell <eivind.sandell@gmail.com>
2016-10-18 13:54:42 +0200

    Fiksed kallet om ulykker i spesifikk kommune slik at det bare kjøres en gang

--- kantega-app/src/Content/search.js
+++ kantega-app/src/Content/search.js
@@ -1,49 +1,59 @@
 var React = require('react');
 import Statdisplay from './statdisplay'
 
-
+//Setter variabler
 var xhttp = new XMLHttpRequest();
 var response = "";
 var kList = []
-// Henter data fra API om kommuner
+var validCall ="no";
+
+// Setter opp API kall
 xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 	response = JSON.parse(this.responseText);
 		}
 	};
 
+//Sender api kall
 xhttp.open("GET", "https://www.vegvesen.no/nvdb/api/v2/omrader/kommuner", false);
 xhttp.send();
 
+//Bygger dictionary med kommuner og kommunenr
 for(var i = 0; i < response.length; i++) {
     var obj = response[i];
 
 	kList[obj.navn] = obj.nummer;
 }
 
+
+// Lager search komponenten
+//-------------------------------------------------------------------------------------------------------------------
 var Search = React.createClass({
+	
   getInitialState: function () {
-  return {userInput: '',correctInput: '', correctID: ''} 
+	return {userInput: '',correctInput: '', correctID: ''} 
   },
 
+  
   // Behandler user input for kommune søkebaren
   handleUserInput: function(e){
 	if (String(e.target.value) in kList){
-		//TODO kjør en funksjon her
 		var inp = e.target.value
 		this.setState({ correctInput: inp })
 		this.setState({ correctID: kList[inp] })
+		validCall = "yes"
+		
 	}
     this.setState({ userInput: e.target.value })
   },
   
-  
+	// Rendrer det som skal til viewet
   render: function () {
     return (
       <div>
 		<p> Søk etter din kommune: </p>
         <input type="text" onChange={this.handleUserInput} value={this.state.userInput} />
-		<Statdisplay kommune={this.state.correctInput} nummer={this.state.correctID}/>
+		<Statdisplay kommune={this.state.correctInput} nummer={this.state.correctID} call={validCall}/>
       </div>
     );
   }
--- kantega-app/src/Content/statdisplay.js
+++ kantega-app/src/Content/statdisplay.js
@@ -1,14 +1,38 @@
 var React = require('react');
 
+var acc = ''
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	acc = JSON.parse(this.responseText);
+		}
+	};
+
+
+function getUlykker(kommunenr, call){
+	if (call == "yes"){
+		xhttp.open("GET", "https://www.vegvesen.no/nvdb/api/v2/vegobjekter/570?kommune="+kommunenr, false);
+		xhttp.send();	
+	}
+}
+
+
+//Lager StatDisplay komponenten
+//---------------------------------------------------------------------------------------------------------------
 var Statdisplay = React.createClass({
+	
   getInitialState: function () {
-  return { kommune: ''} 
+  return { kommune: '', nummer: '', call: 'no'} 
   },
+  
   
   
   render: function () {
     return (
-      <h1> Du har skrevet inm kommune: {this.props.kommune} med nummer: {this.props.nummer}</h1>
+	<div>
+      <h3> Du har skrevet inm kommune: {this.props.kommune} med nummer: {this.props.nummer} </h3>
+	 <p> Ulykker: {getUlykker(this.props.nummer, this.props.call)} </p>
+	</div>
     );
   }
 });
